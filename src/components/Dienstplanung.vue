57701d689c8888d2b811507d75322342d99afde6
max <hello@maxbehr.de>
2017-06-04 12:42:48 +0200

    Modify spacing

--- src/components/Dienstplanung.vue
+++ src/components/Dienstplanung.vue
@@ -1,17 +1,18 @@
 <template>
-  <div id="dienstplanung">
-    <h1 v-text="heading"></h1>
-  </div>
+    <div id="dienstplanung">
+        <h1 v-text="heading"></h1>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'dienstplanung',
-  data () {
-    return {
-      heading: 'Feuerwehr Dienstplanung'
+    name: 'dienstplanung',
+    data () {
+        return {
+            heading: 'Feuerwehr Dienstplanung',
+            employees: null
+        }
     }
-  }
 }
 </script>
 
