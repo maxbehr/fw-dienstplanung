25bcd13802338a88e0139cec514150a286a6b7a4
max <hello@maxbehr.de>
2017-06-06 20:32:54 +0200

    Add vuex

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 global.$ = require('jquery');
 global.jQuery = $;
 
+
 import Vue from 'vue';
 var VueResource = require('vue-resource');
 Vue.use(VueResource);
+
+import Vuex from 'vuex';
+Vue.use(Vuex);
 
 import App from './App.vue';
 
