674528da049211ef82340ff4e5fa86ff1a6f1cc8
max <hello@maxbehr.de>
2017-06-09 21:27:32 +0200

    Toggle display state of Employee List

--- src/components/EmployeeList.vue
+++ src/components/EmployeeList.vue
@@ -1,9 +1,9 @@
 <!-- Template -->
 <template>
     <div id="employee-list">
-        <h5 v-text="options.heading"></h5>
+        <h5 v-text="options.heading" @click="toggleIsOpen"></h5>
 
-        <ul class="employees">
+        <ul class="employees" v-bind:class="{ 'closed': !isOpen }">
             <li @click="selectEmployeeForSeat(employee)" v-for="employee in employees" >
                 <span v-text="employee.firstName"></span>
             </li>
@@ -17,7 +17,7 @@ export default {
     name: 'employeeList',
     data: function() {
         return {
-
+            isOpen: false
         }
     },
     props: [
@@ -32,6 +32,9 @@ export default {
             this.$store.commit('SELECT_EMPLOYEE_FOR_SEAT', { employee: employee })
             this.$emit('employeeWasSelected');
         },
+        toggleIsOpen: function() {
+            this.isOpen = !this.isOpen;
+        }
     }
 }
 </script>
@@ -43,6 +46,8 @@ export default {
         .employees
             list-style-type: none
             padding: 5px 10px
+
+            &.closed { display: none }
 
             li
                 font-size: 0.9em;
